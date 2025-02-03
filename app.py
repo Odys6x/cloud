7f706234c1c9537ccfde41b5da0597a7e8965971
mcspicyupsized <2302689@sit.singaporetech.edu.sg>
2025-02-03 16:30:09 +0800

    key

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from model import ComplexTabularModel
 import altair as alt
 
 
-flask_url = "https://94b8-42-60-47-213.ngrok-free.app/data"
-summary_url = "https://94b8-42-60-47-213.ngrok-free.app/summarize"
+flask_url = "https://d9be-118-200-206-87.ngrok-free.app/data"
+summary_url = "https://d9be-118-200-206-87.ngrok-free.app/summarize"
 
 def fetch_data():
     try:
