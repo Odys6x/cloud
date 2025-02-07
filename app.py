d55055b890ea68b0550f02fc1e615ebae66a415e
mcspicyupsized <2302689@sit.singaporetech.edu.sg>
2025-02-07 23:33:49 +0800

    ngrok

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 from model import ComplexTabularModel
 import altair as alt
 
-flask_url = "https://06e6-42-60-46-140.ngrok-free.app/data"
-summary_url = "https://06e6-42-60-46-140.ngrok-free.app/summarize"
+flask_url = "https://6cd5-42-60-46-140.ngrok-free.app/data"
+summary_url = "https://6cd5-42-60-46-140.ngrok-free.app/summarize"
 
 def fetch_data():
     try:
